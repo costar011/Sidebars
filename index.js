224323100f39413a7629d29bf62ff64270ddb0c4
costar011 <dnaer456@gmail.com>
2024-03-10 22:49:46 +0900

    Js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const subNavs = document.querySelectorAll(`.subnav`);
 const buttons = document.querySelectorAll(`.sidebar button`);
 const resetSidebars = () => {
-    subNavs.forEach((nav) => {
-        nav.style.height = 0;
-    });
+  subNavs.forEach((nav) => {
+    nav.style.height = 0;
+  });
 
-    buttons.forEach(btn => {
-        btn.classList.remove("active");
-    });
-}
+  buttons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+};
 
-const handleClick = subNav => {
-    resetSidebar();
+const handleClick = (subNav) => {
+  resetSidebar();
 
-    const subNavOuter = document.querySelector(`#${subNav}`);
-    const subNavInner = document.querySelector(`#${subNav} .subnav-inner`);
+  const subNavOuter = document.querySelector(`#${subNav}`);
+  const subNavInner = document.querySelector(`#${subNav} .subnav-inner`);
 
-    const button = subNavOuter.previousElementSibling;
+  const button = subNavOuter.previousElementSibling;
 
-    if (subNavOuter.clientHeight > 0) {
-        button.classList.remove("active");
-    }
+  if (subNavOuter.clientHeight > 0) {
+    button.classList.remove("active");
+    subNavOuter.style.height = 0;
+  } else {
+    button.classList.add("active");
+    subNavOuter.style.height = `${subNavInner.clientHeight}px`;
+  }
 };
